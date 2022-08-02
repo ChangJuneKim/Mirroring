954283135833e623749ae888f9013483f7e86d7b
godan6869 <godan6869@naver.com>
2022-08-02 21:16:22 +0900

    데일리 실습 농작물수확하기 제출

--- work_algo/algo_ws_02_3/src/com/ssafy/ws/problem/Solution.java
+++ work_algo/algo_ws_02_3/src/com/ssafy/ws/problem/Solution.java
@@ -4,36 +4,43 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream.GetField;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
+
 	public void solution() throws NumberFormatException, IOException {
-		System.setIn(new FileInputStream("input.txt"));
+//		System.setIn(new FileInputStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder("");
 
 		int T = Integer.valueOf(br.readLine());
 
 		for (int testCase = 1; testCase <= T; testCase++) {
-			int n = Integer.valueOf(br.readLine());
-			int[][] farmData = new int[n][];
+			int N = Integer.valueOf(br.readLine());
+			int[][] farmData = new int[N][];
 
 			// 농장 입력
-			for (int i = 0; i < n; i++) {
+			for (int i = 0; i < N; i++) {
 				farmData[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::valueOf).toArray();
 			}
-
+			
+			int half = N / 2; // 중간지점 
+			
 			int sum = 0;
 			
-			for (int i = n / 2; i < n; i++) {
-				
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					// 맨하튼 거리 내에 있을 경우
+					if (Math.abs(half - r) + Math.abs(half - c) <= half) {
+						sum += farmData[r][c];
+					}
+				}
 			}
-			
-			for (int i = n / 2; i >= 0; i--) {
-				System.out.println(Arrays.toString(farmData[i]));
-			}
+			sb.append(String.format("#%d %d%n", testCase, sum));
 		}
-
+		System.out.print(sb);
 	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
