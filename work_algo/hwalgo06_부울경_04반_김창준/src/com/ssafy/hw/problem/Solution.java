f86b05cf5a5b461a90944d657081cbfa3d86d863
changjune <godan6869@naver.com>
2022-08-08 18:53:11 +0900

    0808 데일리과제 swea d3 9229 한빈이와 Spot Mart 제출

--- work_algo/hwalgo06_부울경_04반_김창준/src/com/ssafy/hw/problem/Solution.java
+++ work_algo/hwalgo06_부울경_04반_김창준/src/com/ssafy/hw/problem/Solution.java
@@ -1,5 +1,8 @@
+package com.ssafy.hw.problem;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+//import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Solution {
@@ -9,7 +12,8 @@ public class Solution {
 	int max;
 
 	public void solution() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int TC = Integer.valueOf(br.readLine());
 
