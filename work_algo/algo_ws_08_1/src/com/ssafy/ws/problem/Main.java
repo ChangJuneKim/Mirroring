4131901a429d87eda67d8ab1badd61eaa007f5ee
changjune <godan6869@naver.com>
2022-08-11 21:33:55 +0900

    0810 데일리실습 boj s1 16926 배열돌리기1 제출

--- work_algo/algo_ws_08_1/src/com/ssafy/ws/problem/Main.java
+++ work_algo/algo_ws_08_1/src/com/ssafy/ws/problem/Main.java
@@ -11,7 +11,6 @@ public class Main {
 	public void solution() throws IOException {
 		System.setIn(new FileInputStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder("");
 
 		String[] NMR = br.readLine().split(" ");
 
@@ -20,7 +19,6 @@ public class Main {
 		int R = Integer.valueOf(NMR[2]);
 
 		int[][] matrix = new int[N][];
-		int[][] answer = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			matrix[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).toArray();
 		}
@@ -33,55 +31,37 @@ public class Main {
 		int[] dx = { 1, 0, -1, 0 };
 		int[] dy = { 0, 1, 0, -1 };
 
-		for (int y = 0; y < M; y++) {
-			for (int x = 0; x < N; x++) {
-				int startX = x;
-				int startY = y;
+		int i, j, d = 0, nr;
 
-				for (int i = 0; i < Math.min(N, M) / 2; i++) {
-					int testX = startX;
-					int testY = startY;
-					int dir = 0;
-					System.out.println("i : " + i);
-					for (int j = 0; j < R; j++) {
-						testX += dx[dir];
-						testY += dy[dir];
-						if (0 + i <= testX && testX < N - i && 0 + i <= testY && testY < M - i) {
-						} else {
-							dir++;
-							if (dir == 4) {
-								dir = 0;
-							}
-							if (dir == 0) {
-								testX++;
-								testY++;
-//								System.out.println(matrix[testX][testY]);
-							} else if (dir == 1) {
-								testX--;
-								testY++;
-//								System.out.println(matrix[testX][testY]);
-							} else if (dir == 2) {
-								testX--;
-								testY--;
-//								System.out.println(matrix[testX][testY]);
-							} else if (dir == 3) {
-								testX++;
-								testY--;
-//								System.out.println(matrix[testX][testY]);
-							}
-						}
-					}
-					answer[testX][testY] = matrix[startX][startY];
-					for (int j = 0; j < answer.length; j++) {
-						System.out.println(Arrays.toString(answer[j]));
-					}
+		for (int idx = 0; idx < Math.min(N, M) / 2; idx++) {
+			nr = R % ((N - idx * 2) * 2 + (M - idx * 2) * 2 - 4); // 새롭게 추가한 코드. 해당하는 테두리의 크기를 modulo 해주어, 반복 횟수를 줄인다.
+			while (nr-- != 0) {
+				i = idx;
+				j = idx;
+				d = 0;
+				int tmp = matrix[i][j];
 
-					startX += 1;
-					startY += 1;
+				while (d != 4) {
+					if (i + dy[d] < idx || i + dy[d] >= N - idx || j + dx[d] < idx || j + dx[d] >= M - idx) {
+						d++;
+						continue;
+					}
+					matrix[i][j] = matrix[i + dy[d]][j + dx[d]];
+					i += dy[d];
+					j += dx[d];
 				}
+				matrix[idx + 1][idx] = tmp;
 			}
 		}
 
+		StringBuilder sb = new StringBuilder();
+		for (int[] row : matrix) {
+			for (int v : row) {
+				sb.append(v).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
 	}
 
 	public static void main(String[] args) throws IOException {
