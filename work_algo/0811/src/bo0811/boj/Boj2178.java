a1a3f6dc92c820f44c6a5459c9d6d9d0aed97d07
changjune <godan6869@naver.com>
2022-08-11 21:32:47 +0900

    0811 목요일 보충수업 순조부 등등 외 백준2178번 문제

--- work_algo/0811/src/bo0811/boj/Boj2178.java
+++ work_algo/0811/src/bo0811/boj/Boj2178.java
@@ -38,27 +38,33 @@ public class Boj2178 {
 
 	private void bfs() {
 		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] { 0, 0 });
-		visit[0][0] = 1;
+		queue.offer(new int[] { 0, 0 }); // 1.큐에 첫 지점을 넣어준다.
+		visit[0][0] = 1; // 1.첫 지점 방문 체크
 
-		while (!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			int r = cur[0];
+		while (!queue.isEmpty()) { // 큐가 빌때까지 반복
+			int[] cur = queue.poll(); // 2.큐에서 하나 뽑는다
+			// 현재 지점 체크
+			int r = cur[0]; 
 			int c = cur[1];
-
+			
+			// 끝까지 갔으면 break
 			if (r == N - 1 && c == M - 1) {
 				break;
 			}
-
+			
+			// 3.사방탐색
 			for (int i = 0; i < 4; i++) {
 				int nr = r + dr[i];
 				int nc = c + dc[i];
+				
+				// 범위 밖이면 가지치기
 				if (!check(nr, nc)) {
 					continue;
 				}
+				// 1 : 길이고, 아직 방문하지 않은곳이면 이동
 				if (map[nr][nc] == 1 && visit[nr][nc] == 0) {
-					queue.offer(new int[] { nr, nc });
-					visit[nr][nc] = visit[r][c] + 1;
+					queue.offer(new int[] { nr, nc }); // 다음 갈 곳을 큐에넣고
+					visit[nr][nc] = visit[r][c] + 1; // visit에 지나온 경로에다가 숫자를 하나씩 쌓으면서 앞으로 간다.
 				}
 			}
 		}
