285338913301c0801618d8581b3c5e18028dc09e
changjune <godan6869@naver.com>
2022-08-16 17:27:09 +0900

    0816 데일리실습 boj s4 2839 설탕배달 제출

--- work_algo/algo_ws_11_1/src/com/ssafy/ws/problem/Main.java
+++ work_algo/algo_ws_11_1/src/com/ssafy/ws/problem/Main.java
@@ -4,19 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-
-	int min = Integer.MAX_VALUE;
-	int N;
-	int[][] ingredients;
 
 	public void solution() throws IOException {
 		System.setIn(new FileInputStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		N = Integer.valueOf(br.readLine());
+		int N = Integer.valueOf(br.readLine());
 		int bag = 0;;
 		
 		while(N >= 0) {
