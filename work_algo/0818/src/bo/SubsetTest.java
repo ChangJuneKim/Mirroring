28ea3ecb4f43b3de3175c841c75ba1775da44d75
changjune <godan6869@naver.com>
2022-08-22 02:17:49 +0900

    0822 부분집합 주석 추가

--- work_algo/0818/src/bo/SubsetTest.java
+++ work_algo/0818/src/bo/SubsetTest.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 // 선택한다? 안한다? 를 계속 곱하기때문에 2^n
 public class SubsetTest {
 
-	static int[] p = { 1, 2, 3, 4, 5 };
-	static int N = p.length;
+	static int[] numbers = { 1, 2, 3, 4, 5 };
+	static int N = numbers.length;
 	static int count;
 	static boolean[] visited;
 	static int total;
@@ -17,6 +17,16 @@ public class SubsetTest {
 
 		subset(0, total);
 		System.out.println(count);
+		
+		System.out.println("바이너리 카운팅을 이용한 부분집합 -> 사전순으로 볼 수 있다.");
+		for (int i = 0; i < (1 << N); i++) {
+			for (int j = 0; j < N; j++) {
+				if((i & (1 << j)) != 0) {
+					System.out.print(numbers[j] + " ");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 	private static void subset(int depth, int total) {
@@ -24,7 +34,7 @@ public class SubsetTest {
 			count++;
 			for (int i = 0; i < N; i++) {
 				if(visited[i]) {
-					System.out.print(p[i] + " ");
+					System.out.print(numbers[i] + " ");
 				}
 			}
 			System.out.println();
@@ -33,7 +43,8 @@ public class SubsetTest {
 		}
 
 		visited[depth] = true;
-		subset(depth + 1, total + p[depth]);
+		subset(depth + 1, total + numbers[depth]);
+		
 		visited[depth] = false;
 		subset(depth + 1, total);
 	}
