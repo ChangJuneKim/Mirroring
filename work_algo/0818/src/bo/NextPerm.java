a2d8dd83e5e622102bbc898afb3caa3edce4be9c
changjune <godan6869@naver.com>
2022-08-22 01:13:03 +0900

    0822 next permutation 설명 주석 추가

--- work_algo/0818/src/bo/NextPerm.java
+++ work_algo/0818/src/bo/NextPerm.java
@@ -3,11 +3,12 @@ package bo;
 import java.util.Arrays;
 
 public class NextPerm {
-	
-	static int[] p = {1,2,3,3,5};
+
+	// !! 오름차순이어야 한다!
+	static int[] p = { 6, 2, 1, 5, 4, 3, 0 };
 	static int N = p.length;
 	static int count;
-	
+
 	public static void main(String[] args) {
 		do {
 			count++;
@@ -15,39 +16,50 @@ public class NextPerm {
 		} while (np(N - 1));
 		System.out.println(count);
 	}
-	
+
 	private static boolean np(int size) {
-		// 1.
-		int i = size;
-		while(i > 0 && p[i - 1] >= p[i]) {
-			i--;
+		int firstPeak = size;
+		// 최초의 하향 변곡점을 찾는다(앞이 뒤보다 작은부분을 찾는 것) ex) 6 2 		"1" 		"5" 		4 3 0
+		// 														(firtPeak - 1) (firstPeak)
+		while (firstPeak > 0 && p[firstPeak - 1] >= p[firstPeak]) {
+			firstPeak--;
 		}
-		// 다 정렬 돼 있다면
-		if(i == 0) {
+
+		// 다 내림차순으로 정렬 돼 있다면 마지막 순열이라는 뜻.
+		if (firstPeak == 0) {
 			return false;
 		}
-		
-		// 2.
-		int j = size;
-		while(p[i - 1] >= p[j]) {
-			j--;
+
+		// 하향 변곡점 앞에 있는 요소와 위치를 바꿀 녀석을 찾기 p[firstPeak - 1] 보다는 큰값이어야 한다
+		// ex) 6 2 "1" 5 4 3 0 -> 여기서는 3이다
+		int gtBeforeFirstPeak = size;
+		while (p[firstPeak - 1] >= p[gtBeforeFirstPeak]) {
+			gtBeforeFirstPeak--;
 		}
-		
-		int temp = p[i - 1];
-		p[i - 1] = p[j];
-		p[j] = temp;
-		
+
+		swap(firstPeak - 1, gtBeforeFirstPeak);
+
 		// 3.
-		int k = size;
-		while(i < k) {
-			temp = p[i];
-			p[i] = p[k];
-			p[k] = temp;
-			
-			i++;
-			k--;
+//		for (int left = firstPeak, right = size; left < right; left++, right--) {
+//			swap(left, right);
+//		}
+
+		// firstPeak(left) 부터 마지막(size)(right) 까지 뒤집는다
+		int right = size;
+		int left = firstPeak;
+		while (left < right) {
+			swap(left, right);
+			left++;
+			right--;
 		}
-		
+
 		return true;
+	}
+
+	static void swap(int a, int b) {
+		int temp = p[a];
+		p[a] = p[b];
+		p[b] = temp;
+
 	}
 }
