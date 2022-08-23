ecc9bf79e98c523970fa58ea0e49b35ae3be5cb0
changjune <godan6869@naver.com>
2022-08-23 09:00:37 +0900

    0823 어제(0822) 보충 코드 수정

--- work_algo/0822/src/bo0822/Solution_2819_bfs.java
+++ work_algo/0822/src/bo0822/Solution_2819_bfs.java
@@ -1,79 +1,84 @@
 package bo0822;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Solution_2819_bfs {
 
-	int T;
-	int N;
-	int[][] map;
-	int[] dx = { -1, 0, 1, 0 };
-	int[] dy = { 0, 1, 0, -1 };
+	static int T;
+	static int N = 4;
+	static int[][] map;
 
-	TreeSet<String> treeSet;
+	static int[] dr = { -1, 0, 1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
 
-	public void solution() throws IOException {
+	static HashSet<String> ts;
+
+	public static void main(String[] args) throws FileNotFoundException {
 		System.setIn(new FileInputStream("input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		T = Integer.valueOf(br.readLine());
-		N = 4;
-
-		for (int testCase = 1; testCase <= T; testCase++) {
+		Scanner scann = new Scanner(System.in);
+		T = scann.nextInt();
+		for (int t = 1; t <= T; t++) {
 			map = new int[N][N];
-			treeSet = new TreeSet<>();
-
+			ts = new HashSet<>();
 			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.valueOf(st.nextToken());
+					map[i][j] = scann.nextInt();
 				}
-			} // 읽기
-
-//			for (int i = 0; i < map.length; i++) {
-//				System.out.println(Arrays.toString(map[i]));
-//			}
-
+			}
+		
 			// 로직
+			// 4x4 (i,j)위치에서 6번 4방 이동해보자.
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					nPr(i, j, 0, new int[] { map[i][j], 0, 0, 0, 0, 0, 0 });
+					bfs(i, j);
 				}
 			}
-
-			System.out.println("#" + testCase + " " + treeSet.size() + "\n");
+			System.out.println("#" + t + " " + ts.size());
 		}
 
 	}
 
-	private void nPr(int x, int y, int depth, int[] nums) {
-		if (depth == 6) {		
-			treeSet.add(Arrays.toString(nums));
-			return;
-		}
-
-		for (int d = 0; d < 4; d++) {
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-
-			if (isIn(nx, ny)) {
-				nums[depth + 1] = map[nx][ny];
-				nPr(nx, ny, depth + 1, nums);
+	// 중복순열 cnt==6까지 반복하자
+	private static void bfs(int r, int c) {
+		// que와 nums를 같이 6번 작업하면 된다.
+		Queue<int[]> que = new LinkedList<>();
+		Queue<String> nums = new LinkedList<>();
+		
+		que.offer(new int[] { r, c, 1 });
+		nums.offer((map[r][c]) + "");
+		
+		while (!que.isEmpty()) {
+			int[] cur = que.poll();
+			String curs = nums.poll();//
+			
+			int cr = cur[0];
+			int cc = cur[1];
+			int cnt = cur[2];
+			
+			if (cnt == 7) {
+				ts.add(curs);
+				continue;
+			}
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = cr + dr[d];
+				int nc = cc + dc[d];
+				
+				if (!check(nr, nc))
+					continue;
+				
+				que.offer(new int[] { nr, nc, cnt + 1 });
+				nums.offer(curs + (map[nr][nc]) + "");
 			}
 		}
 	}
 
-	private boolean isIn(int nx, int ny) {
-		return 0 <= nx && nx < N && 0 <= ny && ny < N;
-	}
-
-	public static void main(String[] args) throws IOException {
-		new Solution_2819_bfs().solution();
+	private static boolean check(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < N;
 	}
 }
