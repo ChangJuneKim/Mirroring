586966b15521f0de80d215ca406cb503e359a727
changjune <godan6869@naver.com>
2022-09-02 16:11:26 +0900

    0902 버그 수정 및 코드 추가

--- work_frontend/0901/front_ws_04_3/js/cafe.js
+++ work_frontend/0901/front_ws_04_3/js/cafe.js
@@ -10,7 +10,7 @@ const $spreadBtn = $storeDisplayBtns[0];
 const $foldBtn = $storeDisplayBtns[1];
 const $vote = document.querySelector('.vote');
 let $backdrop;
-let $modal
+let $modal;
 
 const refreshVoteList = () => {
   $vote.innerHTML = '';
@@ -19,13 +19,9 @@ const refreshVoteList = () => {
       document.querySelector('.no-vote').classList.add('hide');
     }
 
-    const {
-      start_date,
-      end_date,
-      question,
-      answers,
-      expire
-    } = JSON.parse(localStorage.getItem('poll'));
+    const { start_date, end_date, question, answers, expire } = JSON.parse(
+      localStorage.getItem('poll')
+    );
 
     // console.log(start_date, end_date, question, answers, expire)
     if (Date.now() > expire) {
@@ -53,8 +49,10 @@ const refreshVoteList = () => {
       const $input = document.createElement('input');
       $input.setAttribute('type', 'radio');
       $input.setAttribute('name', 'vote-answer');
+      $input.setAttribute('id', answer);
       const $label = document.createElement('label');
       $label.innerText = answer;
+      $label.setAttribute('for', answer);
 
       $li.appendChild($input);
       $li.appendChild($label);
@@ -70,6 +68,7 @@ const refreshVoteList = () => {
     $voteBtn.classList.add('button');
     $voteBtn.classList.add('btn_primary');
     $voteBtn.innerText = '투표하기';
+    $voteBtn.addEventListener('click', vote);
 
     const $resultBtn = document.createElement('button');
     $resultBtn.classList.add('button');
@@ -89,10 +88,16 @@ const refreshVoteList = () => {
     const $noVote = document.createElement('div');
     $noVote.innerHTML = `
     <div class="no-vote">진행중인 투표가 없습니다.</div>
-    `
+    `;
     $vote.appendChild($noVote);
   }
-}
+};
+
+const vote = () => {
+  const votedItem = document.querySelector('input[type=radio]:checked');
+
+  console.log(votedItem);
+};
 
 const toggleAuthState = () => {
   for (const navLink of $navLinks) {
@@ -102,10 +107,9 @@ const toggleAuthState = () => {
       navLink.classList.add('auth');
     }
   }
-}
+};
 
 const spreadList = () => {
-
   for (const storeItemSub of $storeItemSubs) {
     if (storeItemSub.classList.contains('hide')) {
       storeItemSub.classList.add('show');
@@ -117,7 +121,7 @@ const spreadList = () => {
   $spreadBtn.classList.remove('show');
   $foldBtn.classList.add('show');
   $foldBtn.classList.remove('hide');
-}
+};
 
 const foldList = () => {
   for (const storeItemSub of $storeItemSubs) {
@@ -131,7 +135,7 @@ const foldList = () => {
   $spreadBtn.classList.remove('hide');
   $foldBtn.classList.add('hide');
   $foldBtn.classList.remove('show');
-}
+};
 
 const login = () => {
   $profileImages[0].classList.toggle('hide'); //기본 프로필 안보이게
@@ -140,7 +144,7 @@ const login = () => {
   foldList();
   toggleAuthState();
   refreshVoteList();
-}
+};
 
 const logout = () => {
   $profileImages[0].classList.toggle('hide'); //기본 프로필 보이게
@@ -149,7 +153,7 @@ const logout = () => {
   foldList();
   toggleAuthState();
   refreshVoteList();
-}
+};
 
 const closeModal = () => {
   if ($backdrop) {
@@ -159,47 +163,10 @@ const closeModal = () => {
     $modal.remove();
   }
   refreshVoteList();
-}
+};
 
-$login.addEventListener('click', () => {
-  const id = prompt('아이디입력');
-  const password = prompt('비밀번호 입력');
-
-  if (id === 'ssafy' && +password === 1234) {
-    alert('로그인 성공!!!');
-    login();
-  } else {
-    alert('로그인 실패.. 아이디와 비밀번호를 확인하세요.');
-  }
-})
-
-$logout.addEventListener('click', () => {
-  alert('로그아웃 되었습니다.');
-  logout();
-})
-
-$storeItem.addEventListener('click', (e) => {
-  // console.log(e.target) // 이벤트를 발생시키는 요소
-  // console.log(e.currentTarget) // 이벤트 핸들러가 등록된 요소
-  let target = e.target;
-  if (target.tagName === 'A') {
-    target = target.parentElement;
-  }
-
-  for (const child of target.children) {
-    if (child.classList.contains('store_item_sub')) {
-      if (child.classList.contains('hide')) {
-        child.classList.remove('hide');
-        child.classList.add('show');
-      } else {
-        child.classList.remove('show');
-        child.classList.add('hide');
-      }
-    }
-  }
-})
-
-$admin.addEventListener('click', () => {
+// 모달 열기(만들어서 열기)
+const showModal = () => {
   $backdrop = document.createElement('div');
   $backdrop.classList.add('backdrop');
   $backdrop.addEventListener('click', closeModal);
@@ -207,7 +174,6 @@ $admin.addEventListener('click', () => {
   // backdrop.addEventListener('click', closeModal);
 
   $modal = document.createElement('div');
-
 
   $modal.innerHTML = `
     <h3 class="poll-title">투표 만들기</h3>
@@ -237,8 +203,7 @@ $admin.addEventListener('click', () => {
           </div>
         </form>
       </div>
-    `
-
+    `;
 
   $modal.classList.add('modal');
   $modal.classList.add('poll');
@@ -246,14 +211,17 @@ $admin.addEventListener('click', () => {
   const now = new Date();
   const offset = now.getTimezoneOffset() * 60000;
   const today = new Date(now.getTime() - offset).toISOString().substring(0, 10);
-  const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0)
-  const endDay = (new Date(lastDay.getTime() - offset).toISOString().substring(0, 10))
+  const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+  const endDay = new Date(lastDay.getTime() - offset).toISOString().substring(0, 10);
 
   const $startDate = document.querySelector('#start-date');
   $startDate.setAttribute('value', today);
   const $endDate = document.querySelector('#end-date');
   $endDate.setAttribute('value', endDay);
+};
 
+// 투표 후보군 추가하기
+const addList = () => {
   document.querySelector('#btn-add').addEventListener('click', function () {
     const listDiv = document.querySelector('#poll-answer-list');
 
@@ -278,7 +246,10 @@ $admin.addEventListener('click', () => {
     divEl.appendChild(buttonEl);
     listDiv.appendChild(divEl);
   });
+};
 
+// 만들어진 투표 화면에 추가하기
+const makeVote = () => {
   document.querySelector('#btn-make').addEventListener('click', function () {
     let question = document.querySelector('#question').value;
     //console.log(question);
@@ -298,7 +269,22 @@ $admin.addEventListener('click', () => {
 
     let answerArr = [];
     for (let answer of answers) {
-      answerArr.push(answer.value);  // ["서울", ... "부물경"]
+      answerArr.push(answer.value); // ["서울", ... "부물경"]
+    }
+
+    const $startDate = document.querySelector('#start-date');
+    const $endDate = document.querySelector('#end-date');
+    const now = new Date();
+    const offset = now.getTimezoneOffset() * 60000;
+
+    if (!$startDate.value || !$endDate.value) {
+      alert('설문 기간 입력!!!!');
+      return;
+    }
+
+    if ($startDate.value > $endDate.value) {
+      alert('잘못된 날짜 선택');
+      return;
     }
 
     const poll = {
@@ -306,10 +292,10 @@ $admin.addEventListener('click', () => {
       end_date: $endDate.value,
       question: question,
       answers: answerArr,
-      expire: new Date($endDate.value).getTime() - offset
-    }
+      expire: new Date($endDate.value).getTime() - offset,
+    };
 
-    localStorage.setItem('poll', JSON.stringify(poll))
+    localStorage.setItem('poll', JSON.stringify(poll));
 
     if (confirm('투표 생성 하시겠습니까??')) {
       $modal.remove();
@@ -317,14 +303,79 @@ $admin.addEventListener('click', () => {
       refreshVoteList();
     }
   });
-})
-
-$spreadBtn.addEventListener('click', spreadList);
-
-$foldBtn.addEventListener('click', foldList)
+};
 
 const init = () => {
   refreshVoteList();
-}
+};
+
+$login.addEventListener('click', () => {
+  const id = prompt('아이디입력');
+  const password = prompt('비밀번호 입력');
+
+  if (id === 'ssafy' && +password === 1234) {
+    alert('로그인 성공!!!');
+    login();
+  } else {
+    alert('로그인 실패.. 아이디와 비밀번호를 확인하세요.');
+  }
+});
+
+$logout.addEventListener('click', () => {
+  alert('로그아웃 되었습니다.');
+  logout();
+});
+
+$storeItem.addEventListener('click', e => {
+  // console.log(e.target) // 이벤트를 발생시키는 요소
+  // console.log(e.currentTarget) // 이벤트 핸들러가 등록된 요소
+  let target = e.target;
+  if (target.tagName === 'A') {
+    target = target.parentElement;
+  }
+
+  for (const child of target.children) {
+    if (child.classList.contains('store_item_sub')) {
+      if (child.classList.contains('hide')) {
+        child.classList.remove('hide');
+        child.classList.add('show');
+      } else {
+        child.classList.remove('show');
+        child.classList.add('hide');
+      }
+    }
+  }
+
+  const lists = document.querySelector('.store_item').children;
+  let showCount = 0;
+  for (const list of lists) {
+    if (list.children[1].classList.contains('show')) {
+      showCount++;
+    }
+  }
+  if (showCount === document.querySelector('.store_item').childElementCount) {
+    $spreadBtn.classList.add('hide');
+    $spreadBtn.classList.remove('show');
+    $foldBtn.classList.add('show');
+    $foldBtn.classList.remove('hide');
+  }
+
+  if (showCount === 0) {
+    $spreadBtn.classList.add('show');
+    $spreadBtn.classList.remove('hide');
+    $foldBtn.classList.add('hide');
+    $foldBtn.classList.remove('show');
+  }
+});
+
+$admin.addEventListener('click', () => {
+  showModal();
+  addList();
+  makeVote();
+});
+
+$spreadBtn.addEventListener('click', spreadList);
+
+$foldBtn.addEventListener('click', foldList);
 
 init();
