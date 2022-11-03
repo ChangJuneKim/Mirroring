70bda84241f6d87354b699eb5c2a875fd007d69b
changjune <godan6869@naver.com>
2022-11-03 17:56:08 +0900

    1103 뷰 시작

---
+++ work_vuejs/my-vue-test/vueapp/src/components/Test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="test">
+    <h1>{{ title }}</h1>
+    <p v-if="showName">{{ user.firstName }}</p>
+    <p v-else>Nobody</p>
+
+    <ul>
+      <li v-for="item in items" v-bind:key="item.id">{{ item.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      title: "제목",
+      user: {
+        firstName: "John",
+        lastName: "Doe"
+      },
+      showName: false,
+      items: [
+        { id: 1, title: "Item One" },
+        { id: 2, title: "Item Two" },
+        { id: 3, title: "Item Three" },
+        { id: 4, title: "Item Four" },
+        { id: 5, title: "Item Five" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
