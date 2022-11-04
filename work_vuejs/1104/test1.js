68a4ff7e4cc4f62c701622b35a748ae3a23e7dbe
changjune <godan6869@naver.com>
2022-11-04 17:54:00 +0900

    1104 강의실

---
+++ work_vuejs/1104/test1.js
@@ -0,0 +1,17 @@
+// const classes = `header ${
+//   isLargeScreen() ? "" : `icon-${item.isCollapsed ? "expander" : "collapser"}`
+// }`;
+
+// console.log(item);
+
+console.log(item);
+
+var item = {
+  isCollapsed: true,
+};
+
+// function isLargeScreen() {
+//   return false;
+// }
+
+// console.log(classes);
